bb6c0f3686cb9100544f41d7df6d487d336c1e6e
El Mehdi KHALLOUKI <khallouki.elmehdi@gmail.com>
2023-03-06 18:29:58 +0100

    feat(AdsList) - Updated Models Decoding

--- AdForLife/DataProxy/Models/ClassifiedAdModel.swift
+++ AdForLife/DataProxy/Models/ClassifiedAdModel.swift
@@ -8,18 +8,73 @@
 import Foundation
 
 public struct ClassifiedAdImage: Decodable {
-  var small: String?
-  var thumb: String?
+  var small: URL?
+  var thumb: URL?
+  
+  enum CodingKeys: CodingKey {
+    case small
+    case thumb
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    if let smallURLString = try container.decodeIfPresent(String.self,
+                                                          forKey: .small),
+       let smallURL = URL(string: smallURLString) {
+      small = smallURL
+    }
+    if let thumbURLString = try container.decodeIfPresent(String.self,
+                                                          forKey: .thumb),
+       let thumbURL = URL(string: thumbURLString) {
+      thumb = thumbURL
+    }
+  }
 }
 
 public struct ClassifiedAdModel: Decodable {
   var id: Int
   var title: String
-  var category_id: Int
-  var creation_date: String
+  var categoryId: Int
+  var creationDate: Date
   var description: String
-  var is_urgent: Bool
-  var images_url: ClassifiedAdImage
+  var isUrgent: Bool
+  var imagesUrl: ClassifiedAdImage
   var price: Float
   var siret: String?
+  
+  enum CodingKeys: String, CodingKey {
+    case id
+    case categoryId = "category_id"
+    case title
+    case description
+    case price
+    case imagesUrl = "images_url"
+    case creationDate = "creation_date"
+    case isUrgent = "is_urgent"
+    case siret
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    id = try container.decode(Int.self, forKey: .id)
+    categoryId = try container.decode(Int.self, forKey: .categoryId)
+    title = try container.decode(String.self, forKey: .title)
+    description = try container.decode(String.self, forKey: .description)
+    price = try container.decode(Float.self, forKey: .price)
+    imagesUrl = try container.decode(ClassifiedAdImage.self, forKey: .imagesUrl)
+
+    let dateString = try container.decode(String.self, forKey: .creationDate)
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    guard let date = dateFormatter.date(from: dateString) else {
+      throw DecodingError.dataCorruptedError(forKey: .creationDate,
+                                             in: container,
+                                             debugDescription: "Incorrect date format")
+    }
+    self.creationDate = date
+
+    isUrgent = try container.decode(Bool.self, forKey: .isUrgent)
+    siret = try container.decodeIfPresent(String.self, forKey: .siret)
+  }
 }
